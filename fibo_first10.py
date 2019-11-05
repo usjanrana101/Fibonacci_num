98a865a6b0b94b87c46236974e32c8ea483086a6
usjanrana101 <57414182+usjanrana101@users.noreply.github.com>
2019-11-06 02:19:10 +0530

    Add files via upload

---
+++ fibo_first10.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  6 01:16:14 2019
+ Print first 10 fibonacci number
+@author: Jagannath
+"""
+f=[]
+for i in range(10):
+    if i==0:
+        f.append(1)
+    elif i==1:
+        f.append(1)    
+    else:
+        f.append(f[i-2]+f[i-1])
+for i in f:
+    print (i)
+print (f)    

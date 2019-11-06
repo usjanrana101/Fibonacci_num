4fd85bb9e1aacf9203d9c2cded75d3bb38341ecf
usjanrana101 <57414182+usjanrana101@users.noreply.github.com>
2019-11-07 00:15:29 +0530

    importing timeit

--- fibo_first10.py
+++ fibo_first10.py
@@ -3,7 +3,8 @@
 Created on Wed Nov  6 01:16:14 2019
  Print first 10 fibonacci number
 @author: Jagannath
-"""
+""
+import timeit
 f=[]
 for i in range(10):
     if i==0:
@@ -12,6 +13,5 @@ for i in range(10):
         f.append(1)    
     else:
         f.append(f[i-2]+f[i-1])
-for i in f:
-    print (i)
-print (f)    
+print (f)  
+print(timeit.timeit())
